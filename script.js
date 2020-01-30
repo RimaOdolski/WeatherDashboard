7cb2fd3dc9fc033067a1530342108a2418de5d02
Rima O <rymaothmani@gmail.com>
2020-01-30 15:21:00 -0600

    add moments.js to display date and start 5 day forecast function

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 var apiKey = "&appid=e36f2d6c975711e1482f564fe7b024f2";
 var date = new Date();
-var cityName = $("#searchBar").val().trim();
-
-/*$("#searchBar").keypress(function(event) { 
-	
-	if (event.keyCode === 13) { 
-		event.preventDefault();
-		$("#searchBtn").click(); 
-	} 
-}); */
+//var cityName = $("#searchBar").val().trim();
+//var queryForecast= "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + apiKey;
 
 $("#searchBtn").on("click",function() {
+  $("#fivedaycast").addClass("show");
+  console.log("This was a click.");
 
-   // Gets the value of input
+  // Display 5 day forecast 
+  function forecast () {
+    var cityName = $("#searchBar").val().trim();
+    var queryForecast= "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + apiKey;
+    //console.log("This function was called.");
+
+    $.ajax({
+      url: queryForecast,
+      method: "GET"
+    })
+    .then(function (response){
+      //console.log(response);
+      $('#forecast').empty();
+
+      for(var i = 0; i < 5; i++){
+        timeIndex = i * 8 + 7;
+        
+   
+    })
+
+  } 
+
+  forecast();
+
+});
+
+
+ /*  // Gets the value of input
   var cityName = $("#searchBar").val().trim();
 
-
-
-
-    // URL for API call
+   // URL for API call
   var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + apiKey;
     
     //API CALL 
@@ -28,7 +47,7 @@ $("#searchBtn").on("click",function() {
       })
     .then(function (response){
       $('#currentCity').empty();
-
+      //console.log(response);
       // temp in Fahrenheit
        var tempConvert=  Math.floor((response.main.temp - 273.15) * 1.80) + 32;
        var card = $("<div>").addClass("card");
@@ -40,35 +59,43 @@ $("#searchBtn").on("click",function() {
        var humidity = $("<p>").addClass("card-body").text("Humidity: " + response.main.humidity + "%");
        var windSpeed = $("<p>").addClass("card-body").text ("Wind Speed: "+ response.wind.speed + "MPH");
        var icon = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
-      
-      // get the UV Index 
-      var lon = response.coord.lon;
-      var lat = response.coord.lat;
-      queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
-      $.ajax( {
-        url: queryURL,
-        method: "GET",
-        })
-    .then(function(response) {
-        var uvIndex = response.value;
-        var uvColor = "";
-        //var currentUv =$("<p>").text("UV Index: " + uvIndex).attr("style", "background-color: " + uvColor);
-        if(uvIndex < 3){ uvColor = "green";}
-        else if(uvIndex < 6){ uvColor = "yellow";}
-        else if(uvIndex < 8){ uvColor = "orange";}
-        else if(uvIndex < 11){ uvColor = "red";}
-        else{ uvColor = "violet";}
-        $("<p>").text("UV Index: " + uvIndex).attr("style", "background-color: " + uvColor);
          
-    });
        // APPEND TO DOM 
        city.append(icon);
        main.append(city, temperature, humidity, windSpeed);
        card.append(main);
        console.log()
        $("#currentCity").append(card);
-  });
-});
+
+      }); */
+      
+
+
+
+
+
+
+
+ /*/ get the UV Index 
+ var lon = response.coord.lon;
+ var lat = response.coord.lat;
+ queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
+ $.ajax( {
+   url: queryURL,
+   method: "GET",
+   })
+.then(function(response) {
+   var uvIndex = response.value;
+   var uvColor = "";
+   //var currentUv =$("<p>").text("UV Index: " + uvIndex).attr("style", "background-color: " + uvColor);
+   if(uvIndex < 3){ uvColor = "green";}
+   else if(uvIndex < 6){ uvColor = "yellow";}
+   else if(uvIndex < 8){ uvColor = "orange";}
+   else if(uvIndex < 11){ uvColor = "red";}
+   else{ uvColor = "violet";}
+   $("<p>").text("UV Index: " + uvIndex).attr("style", "background-color: " + uvColor);
+
+*/
 
 
 
